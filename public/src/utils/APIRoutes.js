f8308468cadc2b8b9e084639629a57e60915e2b5
Ankitm575 <90247198+Sushmita1111@users.noreply.github.com>
2022-07-17 19:11:48 +0530

    initial commit

---
+++ public/src/utils/APIRoutes.js
@@ -0,0 +1,8 @@
+export const host = "https://wevibe-backend.herokuapp.com";
+export const loginRoute = `${host}/api/auth/login`;
+export const registerRoute = `${host}/api/auth/register`;
+export const logoutRoute = `${host}/api/auth/logout`;
+export const allUsersRoute = `${host}/api/auth/allusers`;
+export const sendMessageRoute = `${host}/api/messages/addmsg`;
+export const recieveMessageRoute = `${host}/api/messages/getmsg`;
+export const setAvatarRoute = `${host}/api/auth/setavatar`;
